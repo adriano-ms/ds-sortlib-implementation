01dc528949bf88dc6e5f05a0ab6dfc7f41015007
adriano-ms <amaciel_sanchez@hotmail.com>
2024-03-18 14:57:31 -0300

    Updated to new answer

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,45 +1,33 @@
 package view;
 
+import java.util.Arrays;
+
 import br.edu.fateczl.sort.Sort;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		int array[] = new int[] {74,20,74,87,81,16,25,99,44,58};
+		int[] ex01 = new int[] {74,20,74,87,81,16,25,99,44,58};
+		int[] ex02 = new int[] {44, 43, 42, 41, 40, 39, 38};
+		int[] ex03 = new int[] {31, 32, 33, 34, 99, 98, 97, 96};
 		
-		int array2[] = new int[] {44,43,42,41,40,39,38};
+		System.out.println("Ex01:");
+		System.out.println("Original: " + Arrays.toString(ex01));
+		Sort.quickSort(ex01);
+		System.out.println("Sorted: " + Arrays.toString(ex01));
+		System.out.println();
 		
-		array = Sort.bubbleSort(array);
-		array2 = Sort.bubbleSort(array2);
+		System.out.println("Ex02:");
+		System.out.println("Original: " + Arrays.toString(ex02));
+		Sort.quickSort(ex02);
+		System.out.println("Sorted: " + Arrays.toString(ex02));
+		System.out.println();
 		
-		System.out.println("Bubble Sort:");
-		System.out.println("Ex.1:");
-		for(int n : array)
-			System.out.print(n + " ");
-		
-		System.out.println("\n\nEx.2:");
-		
-		for(int n : array2)
-			System.out.print(n + " ");
-		
-		array = new int[] {74,20,74,87,81,16,25,99,44,58};
-		
-		array2 = new int[] {44,43,42,41,40,39,38};
-		
-		array = Sort.mergeSort(array, 0, array.length - 1);
-		array2 = Sort.mergeSort(array2, 0, array2.length - 1);
-		
-		System.out.println("\n\nMerge Sort:");
-		System.out.println("Ex.1:");
-		for(int n : array)
-			System.out.print(n + " ");
-		
-		System.out.println("\n\nEx.2:");
-		
-		for(int n : array2)
-			System.out.print(n + " ");
-		
+		System.out.println("Ex03:");
+		System.out.println("Original: " + Arrays.toString(ex03));
+		Sort.quickSort(ex03);
+		System.out.println("Sorted: " + Arrays.toString(ex03));
 		
 	}
 
